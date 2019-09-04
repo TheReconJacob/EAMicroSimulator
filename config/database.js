5c90cf999e8c109a083f36ae2fde13d66832d038
TheReconJacob <jvpokeyjake@gmail.com>
2019-09-04 15:39:54 +0100

    new repo

---
+++ config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'connection':{
+        'host':'localhost',
+        'user':'root',
+        'password':'root'
+    },
+    'database':'nodejs_login',
+    'user_table':'users'
+}
